d027b20a8c119d0d43fd623bad181dd2aab73fdb
Yash Goyal <yashgo0018@gmail.com>
2023-10-29 01:28:13 +0100

    oracle complete

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { ethers } from "ethers";
 import ContractABI from "./abis/ContractABI.json";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "";
+const API_ENDPOINT = process.env.API_ENDPOINT || "";
+const BACKEND_NAME = process.env.BACKEND_NAME || "ibmq_qasm_simulator";
 
 const provider = new ethers.providers.WebSocketProvider(RPC_ENDPOINT);
 
@@ -17,86 +20,102 @@ const contract = new ethers.Contract(CONTRACT_ADDRESS, ContractABI, signer);
 console.log(ethers.utils.toUtf8Bytes("23472398472"));
 
 async function pushCircuit(circuitQasm: string): Promise<string> {
-  // TODO: use the api here
+  try {
+    const {
+      data: { jobId },
+    } = await axios.post(
+      `${API_ENDPOINT}/circuit${
+        BACKEND_NAME ? `?backend_name=${BACKEND_NAME}` : ""
+      }`,
+      {
+        qasm: circuitQasm,
+      }
+    );
 
-  return "123123123124123";
+    return jobId;
+  } catch (err) {
+    console.log(err);
+    return "";
+  }
 }
 
 async function getCircuitOutput(
   jobId: string
 ): Promise<Record<string, number> | null> {
-  // TODO: use the api here
+  const { data } = await axios.post(
+    `${API_ENDPOINT}/result/${jobId}${
+      BACKEND_NAME ? `?backend_name=${BACKEND_NAME}` : ""
+    }`
+  );
 
-  return {
-    "00": 10,
-    "01": 16,
-    "10": 78,
-    "11": 100,
-  };
+  return data;
+}
+
+async function listenForCircuit(jobId: string, circuitHash: string) {
+  console.log("A");
+  getCircuitOutput(jobId)
+    .then(async (output) => {
+      await contract.addData(circuitHash, 2, JSON.stringify(output));
+    })
+    .catch((err) => {
+      console.log(err);
+      listenForCircuit(jobId, circuitHash);
+    });
 }
 
 async function listener() {
   const filter = contract.filters.CircuitAdded();
-  contract.on(filter, async (circuitQASM, circuitHash, event) => {
+  contract.on(filter, async (circuitQASM, circuitHash, _event) => {
     // push the circuit to ibm job list and get a job id, push the job id to the smart contract
     const jobId = await pushCircuit(circuitQASM);
     await contract.addData(circuitHash, 1, jobId);
 
     // start a listener for the response from the server
-    const interval = setInterval(async () => {
-      // check if response is ready and if yes send the response back to the smart contract
-      const output = await getCircuitOutput(jobId);
-
-      if (output) {
-        await contract.addData(circuitHash, 2, JSON.stringify(output));
-        clearInterval(interval);
-      }
-    }, 2000);
+    listenForCircuit(jobId, circuitHash);
   });
 
   const filter2 = contract.filters.ResultsCollected();
-  contract.on(filter2, async (circuitHash, event) => {
-    console.log(circuitHash);
+  contract.on(filter2, async (circuitHash, _event) => {
     // get all the results and average them
     const totalOracleAddresses = await contract.totalOracleAddresses();
-    let data;
+    const responses = [];
+    let max_l = 0;
+    const data: Record<string, number> = {};
+
     for (let i = 0; i < totalOracleAddresses; i++) {
       const response = await contract.oracleResponses(circuitHash, 2, i);
-      const res = JSON.parse(ethers.utils.toUtf8String(response));
-      console.log(res);
-      if (!data) {
-        data = res;
-        continue;
-      }
-
-      for (const i in data) {
+      const res = JSON.parse(response);
+      responses.push(res);
+      for (const key in res) {
         if (
-          Object.prototype.hasOwnProperty.call(data, i) &&
-          Object.prototype.hasOwnProperty.call(res, i)
+          Object.prototype.hasOwnProperty.call(res, key) &&
+          key.length > max_l
         ) {
-          data[i] += res[i];
+          max_l = key.length;
         }
       }
     }
 
-    console.log(data);
-
-    for (const i in data) {
-      if (Object.prototype.hasOwnProperty.call(data, i)) {
-        data[i] = Math.floor(data[i] / Number(totalOracleAddresses));
+    for (const res of responses) {
+      for (let key in res) {
+        if (
+          Object.prototype.hasOwnProperty.call(res, key) &&
+          key.length > max_l
+        ) {
+          res[key.padStart(max_l, "0")] = res[key];
+          delete res[key];
+          key = key.padStart(max_l, "0");
+        }
+        if (!Object.prototype.hasOwnProperty.call(data, key)) {
+          data[key] = res[key];
+        } else {
+          data[key] += res[key];
+        }
       }
     }
 
-    console.log(data);
-
     // push the data as clubed data
-    await contract.addData(
-      circuitHash,
-      3,
-      ethers.utils.toUtf8Bytes(JSON.stringify(data))
-    );
-
-    // console.log(ethers.utils.parseBytes32String(event));
+    await contract.addData(circuitHash, 3, JSON.stringify(data));
   });
 }
 
